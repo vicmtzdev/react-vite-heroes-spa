db2cf81521adbc5b446da8ae18d1e28b25ae5361
Victor Martinez <vicmtzdev@gmail.com>
2023-07-20 14:16:31 -0600

    hero-page

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -19,7 +19,7 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
     return (
         <>
 
-            <div className="col">
+            <div className="col animate__animated animate__fadeIn">
                 <div className="card">
                     <div className="row no-gutters">
 
