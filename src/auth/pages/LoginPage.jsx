060de35a9dc9d37bc858ed54d0e422e0872c83df
Victor Martinez <vicmtzdev@gmail.com>
2023-07-25 23:19:12 -0600

    route-protection

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,19 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
 
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
 
-    navigate('/', { replace: true });
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login('Victor Mtz');
+
+    navigate(lastPath, { replace: true });
 
   }
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 export const AppRouter = () => {
@@ -9,12 +11,25 @@ export const AppRouter = () => {
 
     <>
 
-
       <Routes>  
 
-        <Route path="login" element={<LoginPage />} />
+        <Route path='login' element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
 
-        <Route path="/*" element={<HeroesRoutes />} />
+        {/* <Route path="login" element={<LoginPage />} /> */}
+
+
+
+        <Route path='/*' element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
+
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
 
       </Routes>
 
